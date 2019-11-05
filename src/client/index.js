b11463cca80a8a11d8366b3e2efc38b8fda8d006
Joan Jané <jjaneballester@gmail.com>
2019-11-05 01:33:35 +0100

    Minor fixes

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,5 @@
 export * from './actions.js';
 export * from './ws-client.js';
 export * from './remote-display-client.js';
+export * from './remote-joystick.js';
 export * from './node-web-socket-provider.js';
